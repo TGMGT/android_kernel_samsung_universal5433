2d649376de4e3db6b22c634e0a5f47388bb32ef0
Al Viro <viro@zeniv.linux.org.uk>
2013-06-11 08:23:01 +0400

    allow build_open_flags() to return an error
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/open.c
+++ fs/open.c
@@ -955,25 +955,24 @@ EXPORT_SYMBOL(file_open_root);
 long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 {
 	struct open_flags op;
-	int fd = build_open_flags(flags, mode, &op);
-	struct filename *tmp;
- 	if (fd)
-		return fd;
- 	tmp = getname(filename);
-	if (IS_ERR(tmp))
-		return PTR_ERR(tmp);
- 	fd = get_unused_fd_flags(flags);
-	if (fd >= 0) {
-		struct file *f = do_filp_open(dfd, tmp, &op);
-		if (IS_ERR(f)) {
-			put_unused_fd(fd);
-			fd = PTR_ERR(f);
-		} else {
-			fsnotify_open(f);
-			fd_install(fd, f);
+	int lookup = build_open_flags(flags, mode, &op);
+	struct filename *tmp = getname(filename);
+	int fd = PTR_ERR(tmp);
+
+	if (!IS_ERR(tmp)) {
+		fd = get_unused_fd_flags(flags);
+		if (fd >= 0) {
+			struct file *f = do_filp_open(dfd, tmp, &op, lookup);
+			if (IS_ERR(f)) {
+				put_unused_fd(fd);
+				fd = PTR_ERR(f);
+			} else {
+				fsnotify_open(f);
+				fd_install(fd, f);
+			}
 		}
+		putname(tmp);
 	}
-	putname(tmp);
 	return fd;
 }
 
