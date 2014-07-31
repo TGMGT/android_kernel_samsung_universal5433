4c9a794cd62a6352a1ff8fb3a946bf5d888954af
Alexei Starovoitov <ast@plumgrid.com>
2014-07-30 20:34:15 -0700

    net: filter: rename sk_convert_filter() -> bpf_convert_filter()
    to indicate that this function is converting classic BPF into eBPF
    and not related to sockets
    
    Signed-off-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -392,7 +392,7 @@ static struct seccomp_filter * seccomp_prepare_filter(struct sock_fprog *fprog)
                 goto free_prog;
  
         /* Convert 'sock_filter' insns to 'bpf_insn' insns */
-        ret = sk_convert_filter(fp, fprog->len, NULL, &new_len);
+        ret = bpf_convert_filter(fp, fprog->len, NULL, &new_len);
         if (ret)
                 goto free_prog;
  
