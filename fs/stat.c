561d0b801e3f008f0fabb094f9225bfbec6ce4c6
TGMGT <poop12689@gmail.com>
2026-04-07 12:08:40 +0400

    Add KSU hooks

--- fs/stat.c
+++ fs/stat.c
@@ -18,6 +18,16 @@
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot)) 
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+
+extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
+#endif
+#endif
+
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
 	stat->dev = inode->i_sb->s_dev;
@@ -274,6 +284,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK
+ 	    ksu_handle_stat(&dfd, &filename, &flag);
+    #endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
@@ -286,7 +300,11 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 {
 	struct kstat stat;
 	int error = vfs_fstat(fd, &stat);
-
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK
+	    ksu_handle_newfstat_ret(&fd, &statbuf);
+    #endif
+	
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
 
@@ -406,6 +424,10 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 {
 	struct kstat stat;
 	int error = vfs_fstat(fd, &stat);
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK // for 32-bit
+    	ksu_handle_fstat64_ret(&fd, &statbuf);
+    #endif
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
@@ -418,6 +440,10 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit su
+	    ksu_handle_stat(&dfd, &filename, &flag); 
+    #endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
