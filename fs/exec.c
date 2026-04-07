45125dd9b531280950c148ddf18c64901dbd388a
TGMGT <poop12689@gmail.com>
2026-04-07 12:18:58 +0400

    Add more hooks and fix

--- fs/exec.c
+++ fs/exec.c
@@ -1757,12 +1757,22 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+    __attribute__((hot))
+    extern int ksu_handle_execve(int *fd, const char *filename, void *argv, void *envp, int *flags);
+#endif
+
 int do_execve(const char *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK
+	    ksu_handle_execve((int *)AT_FDCWD, filename, &argv, &envp, 0);
+    #endif
+    
 	return do_execve_common(filename, argv, envp);
 }
 
@@ -1779,6 +1789,11 @@ static int compat_do_execve(const char *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+	
+	#ifdef CONFIG_KSU_MANUAL_HOOK
+	    ksu_handle_execve((int *)AT_FDCWD, filename, &argv, &envp, 0);
+    #endif
+	
 	return do_execve_common(filename, argv, envp);
 }
 #endif
