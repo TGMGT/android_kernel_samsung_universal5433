b862a584a4cb9c746a2c19d911a6e1078b0e52d3
TGMGT <poop12689@gmail.com>
2026-04-13 20:26:48 +0400

    Modify kbase_platform_dvfs_event to accept new parameters
    Updated kbase_platform_dvfs_event function to include additional parameters for GPU utilization sharing.

--- drivers/gpu/arm/t7xx/r22p0/platform/exynos/gpu_dvfs_handler.c
+++ drivers/gpu/arm/t7xx/r22p0/platform/exynos/gpu_dvfs_handler.c
@@ -25,7 +25,7 @@
 extern struct kbase_device *pkbdev;
 
 #ifdef CONFIG_MALI_DVFS
-int kbase_platform_dvfs_event(struct kbase_device *kbdev, u32 utilisation)
+int kbase_platform_dvfs_event(struct kbase_device *kbdev, u32 utilisation, u32 util_gl_share, u32 util_cl_share[2])
 {
 	struct exynos_context *platform;
 
