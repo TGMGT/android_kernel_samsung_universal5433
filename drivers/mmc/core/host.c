81896f2908a85f3db8dcd7f6174b0f5a2ae984a0
TGMGT <poop12689@gmail.com>
2026-04-16 15:22:21 +0400

    Define new thing

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -31,6 +31,7 @@
 #include "host.h"
 
 #define cls_dev_to_mmc_host(d)	container_of(d, struct mmc_host, class_dev)
+#define MMC_CAP2_NO_SDIO_CRC (1 << 10)
 
 static void mmc_host_classdev_release(struct device *dev)
 {
