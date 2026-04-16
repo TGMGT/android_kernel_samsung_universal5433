eb730b6fa165550d14a82ce1dd429c7c17f8a499
TGMGT <poop12689@gmail.com>
2026-04-16 15:23:49 +0400

    Update host.h

--- drivers/mmc/core/host.h
+++ drivers/mmc/core/host.h
@@ -8,6 +8,7 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+#define MMC_CAP2_NO_SDIO_CRC (1 << 10)
 #ifndef _MMC_CORE_HOST_H
 #define _MMC_CORE_HOST_H
 #include <linux/mmc/host.h>
