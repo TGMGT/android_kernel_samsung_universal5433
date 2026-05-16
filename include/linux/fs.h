8c0a68c2203f938f4cf4b19c2bcf033510f37a5f
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-05-16 15:05:53 +0400

    Change dentry open pointer

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1672,7 +1672,7 @@ struct inode_operations {
 			   struct file *, unsigned open_flag,
 			   umode_t create_mode, int *opened);
 	int (*tmpfile) (struct inode *, struct dentry *, umode_t);
-	struct file * (*dentry_open)(struct dentry *, struct vfsmount *, int, const struct cred *);
+	int (*dentry_open)(struct dentry *, struct file *, const struct cred *);
 } ____cacheline_aligned;
 
 ssize_t rw_copy_check_uvector(int type, const struct iovec __user * uvector,
