a5d92cadda49b9304de928a5d0154aef7741f318
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-05-21 16:05:54 +0400

    Add flags parameter to security_path_rename

--- fs/namei.c
+++ fs/namei.c
@@ -4188,7 +4188,7 @@ retry:
 		goto exit5;
 
 	error = security_path_rename(&oldnd.path, old_dentry,
-				     &newnd.path, new_dentry);
+				     &newnd.path, new_dentry, flags);
 	if (error)
 		goto exit5;
 	error = vfs_rename2(oldnd.path.mnt, old_dir->d_inode, old_dentry,
