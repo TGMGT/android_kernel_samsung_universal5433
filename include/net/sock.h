7b777b023e08d57cd57c87edd36b2a697e8b3c26
TGMGT <poop12689@gmail.com>
2026-05-30 21:53:33 +0400

    Remove these defines in sock.h

--- include/net/sock.h
+++ include/net/sock.h
@@ -1629,21 +1629,6 @@ extern void sk_common_release(struct sock *sk);
 /* Initialise core socket variables */
 extern void sock_init_data(struct socket *sock, struct sock *sk);
 
-extern void sk_filter_release_rcu(struct rcu_head *rcu);
-
-/**
- *	sk_filter_release - release a socket filter
- *	@fp: filter to remove
- *
- *	Remove a filter from a socket and release its resources.
- */
-
-static inline void sk_filter_release(struct sk_filter *fp)
-{
-	if (atomic_dec_and_test(&fp->refcnt))
-		call_rcu(&fp->rcu, sk_filter_release_rcu);
-}
-
 /* No longer required added to fitter.h
 static inline void sk_filter_uncharge(struct sock *sk, struct sk_filter *fp)
 {
