0fe82b37de058d8a108ba4dd0d50e9c2977e987a
TGMGT <poop12689@gmail.com>
2026-05-31 14:19:54 +0400

    Change thing

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -414,7 +414,7 @@ bool sk_filter_charge(struct sock *sk, struct sk_filter *fp);
 void sk_filter_uncharge(struct sock *sk, struct sk_filter *fp);
 
 u64 __bpf_call_base(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
-void bpf_int_jit_compile(struct bpf_prog *fp);
+struct bpf_prog *bpf_int_jit_compile(struct bpf_prog *fp);
 
 #ifdef CONFIG_BPF_JIT
 extern int bpf_jit_enable;
