eada6e089e5d4ed55773846a0101446a41d1634a
TGMGT <poop12689@gmail.com>
2026-06-01 15:50:26 +0400

    Refactor set_page_attributes to include pgd and pud checks

--- arch/arm/net/bpf_jit_32.c
+++ arch/arm/net/bpf_jit_32.c
@@ -42,32 +42,35 @@
 static int set_page_attributes(unsigned long virt, int numpages,
 	pte_t (*f)(pte_t))
 {
+	pgd_t *pgd;
+	pud_t *pud;
 	pmd_t *pmd;
 	pte_t *pte;
 	unsigned long start = virt;
 	unsigned long end = virt + (numpages << PAGE_SHIFT);
-	unsigned long pmd_end;
 
 	while (virt < end) {
-		pmd = pmd_off_k(virt);
-		pmd_end = min(ALIGN(virt + 1, PMD_SIZE), end);
-
-		if ((pmd_val(*pmd) & PMD_TYPE_MASK) != PMD_TYPE_TABLE) {
-			pr_err("%s: pmd %p=%08x for %08lx not page table\n",
-				__func__, pmd, (unsigned int)pmd_val(*pmd), virt);
-			virt = pmd_end;
-			continue;
-		}
-
-		while (virt < pmd_end) {
-			pte = pte_offset_kernel(pmd, virt);
-			set_pte_ext(pte, f(*pte), 0);
-			virt += PAGE_SIZE;
-		}
+		pgd = pgd_offset_k(virt);
+		if (pgd_none(*pgd) || pgd_bad(*pgd))
+			break;
+		
+		pud = pud_offset(pgd, virt);
+		if (pud_none(*pud) || pud_bad(*pud))
+			break;
+		
+		pmd = pmd_offset(pud, virt);
+		if (pmd_none(*pmd) || pmd_bad(*pmd))
+			break;
+		
+		pte = pte_offset_kernel(pmd, virt);
+		if (!pte)
+			break;
+		
+		set_pte_ext(pte, f(*pte), 0);
+		virt += PAGE_SIZE;
 	}
 
 	flush_tlb_kernel_range(start, end);
-
 	return 0;
 }
 
