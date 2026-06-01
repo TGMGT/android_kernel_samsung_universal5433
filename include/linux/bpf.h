f7c9d3c8151fffd2be58c957f8ae5e03b8b2559e
TGMGT <poop12689@gmail.com>
2026-06-01 12:11:55 +0400

    Add ARG_PTR_TO_CTX to BPF argument types

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -62,6 +62,8 @@ enum bpf_arg_type {
 	 */
 	ARG_PTR_TO_STACK,	/* any pointer to eBPF program stack */
 	ARG_CONST_STACK_SIZE,	/* number of bytes accessed from stack */
+
+    ARG_PTR_TO_CTX,		/* pointer to context */
 };
 
 /* type of values returned from helper functions */
