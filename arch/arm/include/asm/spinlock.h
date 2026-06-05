793896ae7a4656277a502f38da253d451e768bba
TGMGT <poop12689@gmail.com>
2026-06-05 22:32:32 +0000

    Revert "ARM: 7854/1: lockref: add support for lockless lockrefs using cmpxchg64"
    This reverts commit 97e7c84897e1d3fdb5abd8fa3f7e2c067dd4b8eb.

--- arch/arm/include/asm/spinlock.h
+++ arch/arm/include/asm/spinlock.h
@@ -122,14 +122,10 @@ static inline void arch_spin_unlock(arch_spinlock_t *lock)
 	dsb_sev();
 }
 
-static inline int arch_spin_value_unlocked(arch_spinlock_t lock)
-{
-	return lock.tickets.owner == lock.tickets.next;
-}
-
 static inline int arch_spin_is_locked(arch_spinlock_t *lock)
 {
-	return !arch_spin_value_unlocked(READ_ONCE(*lock));
+	struct __raw_tickets tickets = ACCESS_ONCE(lock->tickets);
+	return tickets.owner != tickets.next;
 }
 
 static inline int arch_spin_is_contended(arch_spinlock_t *lock)
