e7329196296426059a3d68212b0decf68135b052
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-06-11 18:54:36 +0400

    Add BLKDEV_DISCARD_SYNC flag for sync discard

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1067,6 +1067,7 @@ static inline struct request *blk_map_queue_find_tag(struct blk_queue_tag *bqt,
 }
 
 #define BLKDEV_DISCARD_SECURE  0x01    /* secure discard */
+#define BLKDEV_DISCARD_SYNC    0x02    /* sync discard   */
 
 extern int blkdev_issue_flush(struct block_device *, gfp_t, sector_t *);
 extern int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
