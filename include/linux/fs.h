2d6d77ca76600ae07ecef59fcac77027b73e07a6
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-06-11 19:09:04 +0400

    Change kuid_t to uid_t in check_sticky function

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2899,13 +2899,13 @@ int __init get_filesystem_list(char *buf);
  */
 static inline int check_sticky(struct inode *dir, struct inode *inode)
 {
-	kuid_t fsuid = current_fsuid();
+	uid_t fsuid = current_fsuid();
 
 	if (!(dir->i_mode & S_ISVTX))
 		return 0;
-	if (uid_eq(inode->i_uid, fsuid))
+	if (inode->i_uid == fsuid)
 		return 0;
-	if (uid_eq(dir->i_uid, fsuid))
+	if (dir->i_uid == fsuid)
 		return 0;
 	return !capable_wrt_inode_uidgid(inode, CAP_FOWNER);
 }
