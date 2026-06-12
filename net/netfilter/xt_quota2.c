33c3b0994ba18d5414ebfc548ffbdab947a3111e
TouseefX <131637263+TouseefX@users.noreply.github.com>
2026-06-12 22:17:23 +0400

    Refactor quota_list_uid and quota_list_gid parameters

--- net/netfilter/xt_quota2.c
+++ net/netfilter/xt_quota2.c
@@ -52,13 +52,11 @@ static DEFINE_SPINLOCK(counter_list_lock);
 
 static struct proc_dir_entry *proc_xt_quota;
 static unsigned int quota_list_perms = S_IRUGO | S_IWUSR;
-static unsigned int quota_list_uid_param = 0;
-static unsigned int quota_list_gid_param = 0;
+static kuid_t quota_list_uid = KUIDT_INIT(0);
+static kgid_t quota_list_gid = KGIDT_INIT(0);
 module_param_named(perms, quota_list_perms, uint, S_IRUGO | S_IWUSR);
-module_param_named(uid, quota_list_uid_param, uint, S_IRUGO | S_IWUSR);
-module_param_named(gid, quota_list_gid_param, uint, S_IRUGO | S_IWUSR);
-kuid_t quota_list_uid;
-kuid_t quota_list_gid;
+module_param_named(uid, quota_list_uid, uint, S_IRUGO | S_IWUSR);
+module_param_named(gid, quota_list_gid, uint, S_IRUGO | S_IWUSR);
 
 #ifdef CONFIG_NETFILTER_XT_MATCH_QUOTA2_LOG
 static void quota2_log(unsigned int hooknum,
@@ -362,9 +360,6 @@ static int __init quota_mt2_init(void)
 {
 	int ret;
 	pr_debug("xt_quota2: init()");
-
-	quota_list_uid = make_kuid(&init_user_ns, quota_list_uid_param);
-	quota_list_gid = make_kgid(&init_user_ns, quota_list_gid_param);
 
 #ifdef CONFIG_NETFILTER_XT_MATCH_QUOTA2_LOG
 	nflognl = netlink_kernel_create(&init_net, NETLINK_NFLOG, NULL);
